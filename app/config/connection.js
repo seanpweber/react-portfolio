7bf41055acfb9972882b58189d58ed9962189cf6
Sean Weber <sean.weber95@gmail.com>
2021-07-21 14:09:02 -0400

    fixed typo

--- app/config/connection.js
+++ app/config/connection.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const sequelize = new Sequelize(
   heroku_2d2fbea0d3311b2,
   hc1hnvycdvq51eh2,
-  process.env.n08at22zr7wm9m61,
+  n08at22zr7wm9m61,
   {
     host: 'ohunm00fjsjs1uzy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     dialect: 'mysql',
