7a8293504252dbbe434146b2ee3b06b4f3da4642
Sean Weber <sean.weber95@gmail.com>
2021-07-21 14:14:12 -0400

    fixed

--- app/config/connection.js
+++ app/config/connection.js
@@ -2,9 +2,9 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  heroku_2d2fbea0d3311b2,
-  hc1hnvycdvq51eh2,
-  n08at22zr7wm9m61,
+  "heroku_2d2fbea0d3311b2",
+  "hc1hnvycdvq51eh2",
+  "n08at22zr7wm9m61",
   {
     host: 'ohunm00fjsjs1uzy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     dialect: 'mysql',
